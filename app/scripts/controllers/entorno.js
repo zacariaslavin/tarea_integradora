a4414e3fe0f170608cd3e8a977859c82c0787fae
Ignacio Heredia <ignacio.nh@gmail.com>
2018-11-07 09:41:11 -0300

    Using openstreetmap tiles based on config

--- app/scripts/controllers/entorno.js
+++ app/scripts/controllers/entorno.js
@@ -6,23 +6,26 @@ angular.module('obrasMduytApp')
   	$scope.pymChild = new window.pym.Child({ polling: 1000 });
     $scope.pymChild.sendHeight();
 
-
-
-
-
-    var tilesUSIG = {
-                url: '//tiles1.usig.buenosaires.gob.ar/mapcache/tms/1.0.0/amba_con_transporte_3857@GoogleMapsCompatible/{z}/{x}/{y}.png',
-                format: 'tms',
-                builder: 'tms',
-                baseLayer: true,
-                options: {
-                    maxZoom: 18,
-                    minZoom: 9,
-                    attribution:'USIG (<a href="http://www.buenosaires.gob.ar" target="_blank">GCBA</a>), © <a href="http://www.openstreetmap.org/copyright/en" target="_blank">OpenStreetMap</a> (ODbL)',
-                    tms: true
-                }
-              };
-
+    var tilesUSIG;
+    if (window.MDUYT_CONFIG.isCABA) {
+      tilesUSIG = {
+        url: '//tiles1.usig.buenosaires.gob.ar/mapcache/tms/1.0.0/amba_con_transporte_3857@GoogleMapsCompatible/{z}/{x}/{y}.png',
+        format: 'tms',
+        builder: 'tms',
+        baseLayer: true,
+        options: {
+          maxZoom: 18,
+          minZoom: 9,
+          attribution:'USIG (<a href="http://www.buenosaires.gob.ar" target="_blank">GCBA</a>), © <a href="http://www.openstreetmap.org/copyright/en" target="_blank">OpenStreetMap</a> (ODbL)',
+          tms: true
+        }
+      };
+    } else {
+      tilesUSIG = {
+        url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        baseLayer: true
+      };
+    }
 
     $scope.titles = tilesUSIG;
     angular.extend($scope, {
