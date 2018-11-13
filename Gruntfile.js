6b51af1c94bdd938b6794ade791f13704490496e
Ignacio Heredia <ignacio.nh@gmail.com>
2018-11-12 22:15:56 -0300

    Update gruntfile and build

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,6 @@ module.exports = function(grunt) {
       },
       js: {
         files: ["<%= yeoman.app %>/scripts/{,*/}*.js"],
-        tasks: ["newer:jshint:all", "newer:jscs:all"],
         options: {
           livereload: "<%= connect.options.livereload %>"
         }
@@ -531,17 +530,6 @@ module.exports = function(grunt) {
     }
   );
 
-  grunt.registerTask(
-    "server",
-    'DEPRECATED TASK. Use the "serve" task instead',
-    function(target) {
-      grunt.log.warn(
-        "The `server` task has been deprecated. Use `grunt serve` to start a server."
-      );
-      grunt.task.run(["serve:" + target]);
-    }
-  );
-
   grunt.registerTask("test", [
     "clean:server",
     "wiredep",
@@ -561,7 +549,7 @@ module.exports = function(grunt) {
     "concat",
     "ngAnnotate",
     "copy:dist",
-    "cdnify",
+    //"cdnify",
     "cssmin",
     "uglify",
     "filerev",
