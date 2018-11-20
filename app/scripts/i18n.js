f1bd75f98a7277f4f4ebcfe08df1b08c788fdac9
Ignacio Heredia <ignacio.nh@gmail.com>
2018-11-20 08:41:38 -0300

    Starting the i18n config file

---
+++ app/scripts/i18n.js
@@ -0,0 +1,16 @@
+window._i18n = {
+  loading: "Cargando",
+
+  title: "Explorador de obras de SITE_NAME",
+  subtitle: "Esta es la página de prueba para visualizar las obras de SITE_NAME. Click para ver página.",
+
+  homeButton: "Home",
+  searchButton: "Buscador",
+
+  jurisdictionName: "Entorno",
+  seeJurisdictionButton: "Ver Entorno",
+  worksWithoutJurisdictionTitle: "Obras sin entorno",
+
+  homeIframeTitle: "Demo iframe: ",
+  closeButton: "Cerrar"
+};
